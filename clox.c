081df227419c8973b23efdbee32aa74f0018088e
rishi <rishi.veerepalli@gmail.com>
2024-03-31 07:13:52 -0700

    initial

---
+++ clox.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include "common.h"
+#include "chunk.h"
+#include "debug.h"
+
+int
+main(int argc, const char *argv[])
+{
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    disassembleChunk(&chunk, "code");
+
+    freeChunk(&chunk);
+
+    return 0;
+}
